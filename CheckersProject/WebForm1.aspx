3b7f78150588461612c041d4e3404b34dd938191
Itay Shalev <itayshallev@gmail.com>
2023-11-21 10:31:23 +0200

    Added VS project and all needed files

---
+++ CheckersProject/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Checkers.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Checkers</title>
+    <link rel="stylesheet" href="./Style/style.css" />
+    <link rel="icon" type="image/x-icon" href="./Style/icon.ico" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <br />
+        <div>
+            <asp:Button CssClass="submit-button centered-div" Text="Start" runat="server" OnClick="redirectToGame"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CheckersProject/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+namespace Checkers
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string firstName = "";
+        string lastName = "";
+        string email = "";
+        string password = "";
+
+        // ALLOWED USER
+        ////////////////////////////////////////////////
+        string allowedUserEmail = "admin@gmail.com";
+        string allowedUserPassword = "admin";
+        ////////////////////////////////////////////////
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            firstName = Request.QueryString["fname"];
+            lastName = Request.QueryString["lname"];
+            email = Request.QueryString["email"];
+            password = Request.QueryString["password"];
+
+            Response.Write("<div class='centered-div regular-text' style='top: 30%'>" +
+                "Welcome " + firstName + " " + lastName +
+                "</div>");
+        }
+
+        protected void redirectToGame(object sender, EventArgs e)
+        {
+            if (email == allowedUserEmail && password == allowedUserPassword)
+            {
+                Response.Redirect("./Home/home.html");
+            }
+            else
+            {
+                Response.Write("<div class='centered-div regular-text' style='top:35%; color: red'>Error: User Not Found</div>");
+
+                //Thread.Sleep(2000);
+                //Response.Redirect("./Register/Register.html");
+            }
+        }
+    }
+}
